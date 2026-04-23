ce64231ceba2bdfc91939dd53aeabc61427792d9
ls <liushuo_fe@163.com>
2026-04-23 08:52:13 +0800

    feat(jsx基础): 识别 js 表达式

--- oReact/01-react-basic/src/App.js
+++ oReact/01-react-basic/src/App.js
@@ -1,8 +1,27 @@
 // 项目的根组件
 // APP --> index.js --> public/index.html(root)
 
+const count = 100;
+
+function getName() {
+  return "jack";
+}
 function App() {
-  return <div className="App">this is App</div>;
+  return (
+    <div className="App">
+      this is App
+      {/* 使用引号传递字符串 */}
+      {"this is a message"}
+      {/* 识别JS变量 */}
+      {count}
+      {/* 函数调用 */}
+      {getName()}
+      {/* 方法调用 */}
+      {new Date().getDate()}
+      {/* 使用js对象 */}
+      <div style={{color: 'red'}}>this is div</div>
+    </div>
+  );
 }
 
 export default App;
