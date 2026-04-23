7e4814c55f1845be3a76a9762e5581d9cb6e1543
ls <liushuo_fe@163.com>
2026-04-23 09:11:06 +0800

    feat(jsx基础): 实现列表渲染

--- oReact/01-react-basic/src/App.js
+++ oReact/01-react-basic/src/App.js
@@ -1,15 +1,24 @@
 // 项目的根组件
 // APP --> index.js --> public/index.html(root)
 
+// 1、jsx基础 -- 识别js表达式
 const count = 100;
 
 function getName() {
   return "jack";
 }
+
+// 2、jsx基础 -- 实现列表渲染
+const list = [
+  { id: 1001, name: "Vue" },
+  { id: 1002, name: "React" },
+  { id: 1003, name: "Angular" },
+];
 function App() {
   return (
     <div className="App">
       this is App
+      {/* jsx基础 -- 识别js表达式 */}
       {/* 使用引号传递字符串 */}
       {"this is a message"}
       {/* 识别JS变量 */}
@@ -19,7 +28,17 @@ function App() {
       {/* 方法调用 */}
       {new Date().getDate()}
       {/* 使用js对象 */}
-      <div style={{color: 'red'}}>this is div</div>
+      <div style={{ color: "red" }}>this is div</div>
+      {/* 2、jsx基础 -- 实现列表渲染 */}
+      {/* 渲染列表 */}
+      {/* map 循环哪个结构 return 哪个结构 */}
+      {/* 注意事项：加上一个独一无二的key string | number */}
+      {/* key的作用：React框架内部使用 提升更新性能的 */}
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
