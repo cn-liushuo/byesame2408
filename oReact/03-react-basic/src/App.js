78b2bfa960c656b6ffec4f85c0711cf3ae70cea9
ls <liushuo_fe@163.com>
2026-05-12 11:13:03 +0800

    feat: 受控绑定表单

--- oReact/03-react-basic/src/App.js
+++ oReact/03-react-basic/src/App.js
@@ -1,7 +1,22 @@
-const App = () => (
-  <>
-    <h1>Hello World</h1>
-  </>
-)
+//1、受控绑定表单
+import { useState } from "react";
+
+// 1.1、声明一个 react 状态 - useState
+
+// 1.2、核心绑定流程
+// 1.2.1、通过 value 属性绑定 react 状态
+// 1.2.2、绑定 onChange 事件 通过事件参数 e 拿到输入框最新的值 反向修改到 react 状态身上
+const App = () => {
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+      ></input>
+    </>
+  );
+};
 
 export default App;
