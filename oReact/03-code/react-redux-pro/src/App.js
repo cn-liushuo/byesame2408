8b681b797aab1ba7101e321d5cf1e79c89e3ca97
ls <liushuo_fe@163.com>
2026-05-15 14:14:34 +0800

    feat: redux 与 react - 异步状态操作;

--- oReact/03-code/react-redux-pro/src/App.js
+++ oReact/03-code/react-redux-pro/src/App.js
@@ -3,10 +3,17 @@
 import { useSelector, useDispatch } from "react-redux";
 // 导入 actionCreator
 import { increment, decrement, addToNum } from "./store/modules/counterStore";
+import { useEffect } from "react";
+import { fetchChannelList } from "./store/modules/channelStore";
 
 function App() {
   const { count } = useSelector((state) => state.counter);
+  const { channelList } = useSelector((state) => state.channel);
   const dispatch = useDispatch();
+  // 使用 useEffect 触发异步请求执行
+  useEffect(() => {
+    dispatch(fetchChannelList());
+  }, [dispatch]);
   return (
     <div className="App">
       <button onClick={() => dispatch(decrement())}>-</button>
@@ -14,6 +21,11 @@ function App() {
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(addToNum(10))}>add To 10</button>
       <button onClick={() => dispatch(addToNum(20))}>add To 20</button>
+      <ul>
+        {channelList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
