f64bfd03c489f209a9323955db72ea92243eba0b
ls <liushuo_fe@163.com>
2026-05-19 16:22:14 +0800

    feat(react-router-pro): 嵌套路由配置

---
+++ oReact/03-code/react-router-pro/src/page/Board/index.js
@@ -0,0 +1,9 @@
+const Board = () => {
+  return (
+    <>
+      <div>我是面板页</div>
+    </>
+  );
+};
+
+export default Board;
---
+++ oReact/03-code/react-router-pro/src/page/Layout/index.js
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <div>
+        我是一级路由 layout 组件
+        <Link to="/board">面板</Link>
+        <Link to="/about">关于</Link>
+        {/* 配置二级路由的出口 */}
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
