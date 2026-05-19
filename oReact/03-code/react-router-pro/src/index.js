965f3385fe32dc4176077a1d026afe9b6c395001
ls <liushuo_fe@163.com>
2026-05-19 14:58:25 +0800

    feat(react-router-pro): 最简单的路由

--- oReact/03-code/react-router-pro/src/index.js
+++ oReact/03-code/react-router-pro/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 1、创建 router 实例对象并且配置路由对应关系
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <div>我是登录页</div>,
+  },
+  {
+    path: "/article",
+    element: <div>我是文章页</div>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* 2、路由绑定 */}
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
