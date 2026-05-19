7967ec7b7eaf3734b7441762b55120db24741e90
ls <liushuo_fe@163.com>
2026-05-19 16:07:58 +0800

    feat(react-router-pro): 路由跳转

--- oReact/03-code/react-router-pro/src/page/Login/index.js
+++ oReact/03-code/react-router-pro/src/page/Login/index.js
@@ -1,7 +1,18 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <div>我是登录页</div>
+      <div>
+        我是登录页
+        {/* 声明式导航 */}
+        {/* 声明式写法 */}
+        <Link to={"/article"}>跳转到文章页</Link>
+        {/* 编程式导航 */}
+        {/* 命令式写法 */}
+        <button onClick={() => navigate("/article")}>跳转到文章页</button>
+      </div>
     </>
   );
 };
