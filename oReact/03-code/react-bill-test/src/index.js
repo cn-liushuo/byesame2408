02559311fba3d01a315c1e593ca2dacfc7e1edae
ls <liushuo_fe@163.com>
2026-05-20 15:51:39 +0800

    feat(react-bill-pro): 使用 rtk 管理账单列表

--- oReact/03-code/react-bill-test/src/index.js
+++ oReact/03-code/react-bill-test/src/index.js
@@ -7,9 +7,15 @@ import router from "./router";
 
 // 导入定制主体文件
 import "./theme.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const total = sum(1, 2);
 console.log("total", total);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
