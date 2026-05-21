7d5a96bcfdd9a48f3f13be4e0a145fc06042f4a4
ls <liushuo_fe@163.com>
2026-05-21 10:57:02 +0800

    feat(react-bill-test): tabber实现

--- oReact/03-code/react-bill-test/src/page/Layout/index.js
+++ oReact/03-code/react-bill-test/src/page/Layout/index.js
@@ -1,27 +1,54 @@
-import { Button } from "antd-mobile";
-import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { getBillList } from "@/store/modules/billStore";
+import { TabBar } from 'antd-mobile'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+import './index.scss'
+
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getBillList());
-  }, [dispatch]);
-  return (
-    <>
-      <div>
-        <Outlet />
-        我是 Layout
-        {/* 测试全局生效样式 */}
-        <Button color="primary">测试全局</Button>
-        <div className="purple">
-          <Button color="primary">测试局部</Button>
-        </div>
-      </div>
-    </>
-  );
-};
+  const location = useLocation()
+  
+  // 切换菜单跳转路由
+  const navigate = useNavigate()
 
-export default Layout;
+  return (
+    <div className="Layout">
+      <div className="page">
+        {/* 二级路由出口 */}
+        <Outlet />
+      </div>
+
+      <TabBar
+        className="tabbar"
+        activeKey={location.pathname}
+        onChange={key => navigate(key)}
+      >
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+    </div>
+  )
+}
+
+export default Layout
